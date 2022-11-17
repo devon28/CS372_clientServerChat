f933560a2bdce4dc229c233ceb9c16a895d6fd83
devon28 <devonmiller4623@gmail.com>
2022-11-17 13:14:14 -0500

    finished with flake8

--- client.py
+++ client.py
@@ -1,36 +1,47 @@
-# Description: This file holds the client portion of a client server chat program client and
-#               server send messages to each other over a single socket connection until one of
-#               the parties closes the chat
+# Author: Devon Miller
+# Date: 11/25/2022
+# Course: CS 372
+# Assignment: Project 4: Client Server Chat
+# Description: This file holds the client portion of a client server chat
+#               program client and server send messages to each other over
+#               a single socket connection until one of the parties closes
+#               the chat
 
 import socket
 
+
 class Client():
-
+    """instance of client to interact with Server() class"""
     def client(self):
-
-        HOST = "127.0.0.1"  # local host
-        PORT = 16022  # can be any port > 1023
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # TCP connection
+        """client connects to local host and same port as Server() then
+        sends messages back and forth taking turns in a game of 20 questions"""
+        HOST = "127.0.0.1"               # local host
+        PORT = 16022                     # same as Server()
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         connection.connect((HOST, PORT))
-        print("Welcome to 20 questions, the server thought of a person place or thing and you need go guess!")
+        print("Welcome to 20 questions, the server thought of a person place \
+        or thing and you need go guess!")
         print("on your turn enter /q to quit")
         print("Opponent chosing something to guess.......")
         self.__connect(connection)
 
     def __connect(self, connection):
+        """private function for a single connection called by client
+        function"""
         while True:
-            response = connection.recv(1024)  # one recv is sufficient to read all data here
+            response = connection.recv(1024)
             print(response.decode())
-            if b"/q" in response:
+            if b"/q" in response:    # quit
                 connection.close()
                 break
             data = input(">>> ")
             data = bytes(data, 'utf-8')
-            if data == b"/q":
+            if data == b"/q":   # quit
                 connection.send(data)
                 connection.close()
                 break
             connection.sendall(data)  # data to display in browser
+
 
 if __name__ == "__main__":
     client = Client()
--- server.py
+++ server.py
@@ -1,56 +1,71 @@
-# Description: This file holds the server portion of a client server chat program client and
-#               server send messages to each other over a single socket connection until one of
-#               the parties closes the chat
+# Author: Devon Miller
+# Date: 11/25/2022
+# Course: CS 372
+# Assignment: Project 4: Client Server Chat
+# Description: This file holds the server portion of a client server chat
+#               program client and server send messages to each other over
+#               a single socket connection until one of parties closes the chat
 
 class Server():
+    """server class holds instance of server to connect with Client() class"""
     def server(self):
-
+        """binds to port and local host then communicates back and forth with
+        client in a game of 20 questions where the client must guess an object
+        of the servers choosing"""
         import socket
-        HOST = "127.0.0.1"                                    # local host
-        PORT = 16022                                          # can be any port > 1023
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:     # TCP connection
+        HOST = "127.0.0.1"       # local host
+        PORT = 16022             # same port as Client()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
             socket.bind((HOST, PORT))
             socket.listen()
             connection, address = socket.accept()
             print(f"Connected by {address}")
-            print("welcome to 20 questions, as the server you'll choose an object the client must guess!")
-            goal = input("what will the client try to guess?\n")
+            print("welcome to 20 questions, as the server you'll choose an \
+            object the client must guess!")
+            goal = input("what will the client try to guess?\n")  # string to guess
             goal = bytes(goal, 'utf-8')
             print("Lets begin!")
             print("on your turn enter /q to quit")
-            guesses = 20
+            guesses = 19           # number of guesses to guess goal
             data = (b"The game has begun, you have %d guesses left" % guesses)
             connection.send(data)
             self.__connect(connection, goal, guesses)
         ""
     def __connect(self, connection, goal, guesses):
+        """A connection handled by server class, connection is the connection
+        goal is the string to guess, guesses are the number of remaining
+        guesses"""
         while True:
-            response = connection.recv(1024)  # one recv is sufficient to read all data here
+            response = connection.recv(1024)
             print(response.decode())
-            if b"/q" in response:
+            if b"/q" in response:     # quit
                 connection.close()
                 break
-            if response.lower() == goal.lower():
+            if response.lower() == goal.lower():   # client wins
                 self.__clientWinner(connection)
                 break
-            if guesses == 0:
+            if guesses == 0:      # server wins
                 self.__serverWinner(connection)
                 break
             data = input(">>>>>>> ")
-            if data == "/q":
+            if data == "/q":    # quit
                 self.__quit(connection)
                 break
-            data = bytes(data + ", you have %d guesses left" % guesses, 'utf-8')
+            data = bytes(data + ", you have %d guesses \
+            left" % guesses, 'utf-8')
             connection.sendall(data)
             guesses -= 1
 
     def __clientWinner(self, connection):
+        """client wins, prints messages to client and server then closes
+        connection on both sides"""
         print("The client won!")
         data = b"Youre the winner great job! /q"
         connection.send(data)
         connection.close()
 
     def __serverWinner(self, connection):
+        """server wins, client unable to guesss object in 20 guesses"""
         print("The client ran out of guesses you've won!")
         data = b"You ran out of guesses, YOU LOST!"
         connection.send(data)
@@ -58,8 +73,10 @@ class Server():
         connection.close()
 
     def __quit(self, connection):
+        """closes connection on server and client side"""
         connection.send(b"/q")
         connection.close()
+
 
 if __name__ == "__main__":
     server = Server()
