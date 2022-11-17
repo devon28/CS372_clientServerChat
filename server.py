e41773074be59513f5f51ac0c3e7b1ca0deb5cae
devon28 <devonmiller4623@gmail.com>
2022-11-17 09:34:26 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,7 @@
+# Author: Devon Miller
+# Date: 11/25/2022
+# Course: CS 372
+# Assignment: Project 4: Client Server Chat
+# Description: This file holds the server portion of a client server chat program client and
+#               server send messages to each other over a single socket connection until one of
+#               the parties closes the chat
