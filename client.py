4d2319dcc61cdcbb5739edfbf5feb5a01b6dea76
devon28 <devonmiller4623@gmail.com>
2022-11-17 11:02:37 -0500

    working 20 questions need to add byte length

--- client.py
+++ client.py
@@ -5,3 +5,32 @@
 # Description: This file holds the client portion of a client server chat program client and
 #               server send messages to each other over a single socket connection until one of
 #               the parties closes the chat
+
+import socket
+
+def client():
+    """ Makes socket connection to gaia.cs.umass.edu, sends GET request, prints response
+    to GET request and closes socket connection"""
+
+    HOST = "127.0.0.1"  # local host
+    PORT = 16022  # can be any port > 1023
+
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # TCP connection
+    connection.connect((HOST, PORT))
+    print("Welcome to 20 questions, the server thought of a person place or thing and you need go guess!")
+    print("on your turn enter /q to quit")
+    print("Opponent chosing something to guess.......")
+    while True:
+        response = connection.recv(1024)  # one recv is sufficient to read all data here
+        print(response.decode())
+        if response == "/q":
+            connection.close()
+        data = input(">>> ")
+        data = bytes(data, 'utf-8')
+        if data == b"/q":
+            connection.send(data)
+            connection.close()
+        connection.sendall(data)  # data to display in browser
+
+if __name__ == "__main__":
+    client()
--- server.py
+++ server.py
@@ -5,3 +5,59 @@
 # Description: This file holds the server portion of a client server chat program client and
 #               server send messages to each other over a single socket connection until one of
 #               the parties closes the chat
+
+def server():
+    """ Simple server that accepts connections, prints response to terminal, sends html
+    data viewable through browser and prints this data to the terminal"""
+
+    import socket
+    HOST = "127.0.0.1"                                    # local host
+    PORT = 16022                                          # can be any port > 1023
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:     # TCP connection
+        socket.bind((HOST, PORT))                        # bind host and port to allow socket connections
+        socket.listen()
+        connection, address = socket.accept()            # connection is socket connection, adress is connected machine
+        #with connection:
+        print(f"Connected by {address}")
+        print("welcome to 20 questions, as the server you'll choose an object the client must guess!")
+        goal = input("what will the client try to guess?\n")
+        goal = bytes(goal, 'utf-8')
+        print("Lets begin!")
+        print("on your turn enter /q to quit")
+        guesses = 19
+        data = (b"The game has begun, you have %d guesses left" % guesses)
+        connection.send(data)
+        while True:
+            #print(f"Connected by {address}")
+            response = connection.recv(1024)             # one recv is sufficient to read all data here
+            print(response.decode())
+            if response == "/q":
+                connection.close()
+            print(True, response, goal)
+            if response == goal:
+                print("The client won!")
+                data = b"Youre the winner great job!"
+                connection.send(data)
+                connection.send(b"/q")
+                connection.close()
+            if guesses == 0:
+                print("The client ran out of guesses you've won!")
+                data = b"You ran out of guesses, YOU LOST!"
+                connection.send(data)
+                connection.send(b"/q")
+                connection.close()
+            data = input("(yes or no >>>>>>> ")
+            print(data)
+            if data == b"/q":
+                connection.send(data)
+                connection.close()
+            data = bytes(data + ", you have %d guesses left" % guesses, 'utf-8')
+            connection.sendall(data)
+            guesses -= 1
+
+            #print("Sending>>>>>>>>")
+
+
+if __name__ == "__main__":
+    server()
