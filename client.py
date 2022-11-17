ae4cc8070146b165b6751e507d48550f5f7a917c
devon28 <devonmiller4623@gmail.com>
2022-11-17 12:42:50 -0500

    all working not commented

--- client.py
+++ client.py
@@ -4,32 +4,34 @@
 
 import socket
 
-def client():
-    """ Makes socket connection to gaia.cs.umass.edu, sends GET request, prints response
-    to GET request and closes socket connection"""
+class Client():
 
-    HOST = "127.0.0.1"  # local host
-    PORT = 16022  # can be any port > 1023
+    def client(self):
 
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # TCP connection
-    connection.connect((HOST, PORT))
-    print("Welcome to 20 questions, the server thought of a person place or thing and you need go guess!")
-    print("on your turn enter /q to quit")
-    print("Opponent chosing something to guess.......")
-    #connect(connection)
-    while True:
-        response = connection.recv(1024)  # one recv is sufficient to read all data here
-        print(response.decode())
-        if response == b"/q":
-            connection.close()
-            break
-        data = input(">>> ")
-        data = bytes(data, 'utf-8')
-        if data == b"/q":
-            connection.send(data)
-            connection.close()
-            break
-        connection.sendall(data)  # data to display in browser
+        HOST = "127.0.0.1"  # local host
+        PORT = 16022  # can be any port > 1023
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # TCP connection
+        connection.connect((HOST, PORT))
+        print("Welcome to 20 questions, the server thought of a person place or thing and you need go guess!")
+        print("on your turn enter /q to quit")
+        print("Opponent chosing something to guess.......")
+        self.__connect(connection)
+
+    def __connect(self, connection):
+        while True:
+            response = connection.recv(1024)  # one recv is sufficient to read all data here
+            print(response.decode())
+            if b"/q" in response:
+                connection.close()
+                break
+            data = input(">>> ")
+            data = bytes(data, 'utf-8')
+            if data == b"/q":
+                connection.send(data)
+                connection.close()
+                break
+            connection.sendall(data)  # data to display in browser
 
 if __name__ == "__main__":
-    client()
+    client = Client()
+    client.client()
--- server.py
+++ server.py
@@ -2,62 +2,65 @@
 #               server send messages to each other over a single socket connection until one of
 #               the parties closes the chat
 
-def server():
-    """ Simple server that accepts connections, prints response to terminal, sends html
-    data viewable through browser and prints this data to the terminal"""
+class Server():
+    def server(self):
 
-    import socket
-    HOST = "127.0.0.1"                                    # local host
-    PORT = 16022                                          # can be any port > 1023
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:     # TCP connection
-        socket.bind((HOST, PORT))                        # bind host and port to allow socket connections
-        socket.listen()
-        connection, address = socket.accept()            # connection is socket connection, adress is connected machine
-        #with connection:
-        print(f"Connected by {address}")
-        print("welcome to 20 questions, as the server you'll choose an object the client must guess!")
-        goal = input("what will the client try to guess?\n")
-        goal = bytes(goal, 'utf-8')
-        print("Lets begin!")
-        print("on your turn enter /q to quit")
-        guesses = 18
-        data = (b"The game has begun, you have %d guesses left" % guesses)
-        connection.send(data)
+        import socket
+        HOST = "127.0.0.1"                                    # local host
+        PORT = 16022                                          # can be any port > 1023
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:     # TCP connection
+            socket.bind((HOST, PORT))
+            socket.listen()
+            connection, address = socket.accept()
+            print(f"Connected by {address}")
+            print("welcome to 20 questions, as the server you'll choose an object the client must guess!")
+            goal = input("what will the client try to guess?\n")
+            goal = bytes(goal, 'utf-8')
+            print("Lets begin!")
+            print("on your turn enter /q to quit")
+            guesses = 20
+            data = (b"The game has begun, you have %d guesses left" % guesses)
+            connection.send(data)
+            self.__connect(connection, goal, guesses)
+        ""
+    def __connect(self, connection, goal, guesses):
         while True:
-            #print(f"Connected by {address}")
-            response = connection.recv(1024)             # one recv is sufficient to read all data here
+            response = connection.recv(1024)  # one recv is sufficient to read all data here
             print(response.decode())
-            if response == b"/q":
+            if b"/q" in response:
                 connection.close()
                 break
-            #print(True, response, goal)
-            if response == goal:
-                print("The client won!")
-                data = b"Youre the winner great job!"
-                connection.send(data)
-                connection.send(b"/q")
-                connection.close()
+            if response.lower() == goal.lower():
+                self.__clientWinner(connection)
+                break
             if guesses == 0:
-                print("The client ran out of guesses you've won!")
-                data = b"You ran out of guesses, YOU LOST!"
-                connection.send(data)
-                connection.send(b"/q")
-                connection.close()
-            data = input(">>>>>>> ")
-            #print(data)
-            print(data)
-            if data == "/q":
-                print(True, False, True)
-                connection.send(b"/q")
-                connection.close()
+                self.__serverWinner(connection)
                 break
-            data = bytes(data + ", you have %d guesses left" % int(guesses+1), 'utf-8')
+            data = input(">>>>>>> ")
+            if data == "/q":
+                self.__quit(connection)
+                break
+            data = bytes(data + ", you have %d guesses left" % guesses, 'utf-8')
             connection.sendall(data)
             guesses -= 1
 
-            #print("Sending>>>>>>>>")
+    def __clientWinner(self, connection):
+        print("The client won!")
+        data = b"Youre the winner great job! /q"
+        connection.send(data)
+        connection.close()
 
+    def __serverWinner(self, connection):
+        print("The client ran out of guesses you've won!")
+        data = b"You ran out of guesses, YOU LOST!"
+        connection.send(data)
+        connection.send(b"/q")
+        connection.close()
+
+    def __quit(self, connection):
+        connection.send(b"/q")
+        connection.close()
 
 if __name__ == "__main__":
-    server()
+    server = Server()
+    server.server()
